9414a3c00bffedb8c96d89c5f1ab030d201d8c9d
zhangjun <zhangjun@ehousechina.com>
2016-12-02 18:43:40 +0800

    little fix..

--- app/src/main/java/com/lovejjfg/swiperefreshrecycleview/Pick2Activity.java
+++ app/src/main/java/com/lovejjfg/swiperefreshrecycleview/Pick2Activity.java
@@ -1,9 +1,12 @@
 /*
- * Copyright (c) 2016.  Joe<lovejjfg@163.com>
+ * Copyright (c) 2016.  Joe
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -100,10 +103,6 @@ public class Pick2Activity extends AppCompatActivity {
     static class PickAdapter extends SelectRefreshRecycleAdapter<PickedBean> {
         public PickAdapter() {
             super(ISelect.SingleMode, false);
-        }
-
-        public PickAdapter(int currentMode, boolean longTouchEnable) {
-            super(currentMode, longTouchEnable);
         }
 
         @Override
